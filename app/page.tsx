275048ce399288d890a60f3503657487537af156
unknown <amazighmalek.com>
2025-03-06 16:10:40 +0100

    prisma client getData

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,36 @@
+import { prisma } from "@/lib/db";
 
-export default function Home() {
+async function GetData() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const data = await prisma.blogPost.findMany({
+    select: {
+      title: true,
+      content: true,
+      imageUrl: true,
+      authorImage: true,
+      authorName: true,
+      id: true,
+      createdAt: true,
+      authorId: true,
+      updatedAt: true,
+    },
+  });
+
+  return data;
+}
+export default async function Home() {
+  const data= await GetData()
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <h1 className="text-3xl">Hello Alphazero</h1>
+    <div className="py-6 mb-8 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <h1 className="text-3xl font-bold tracking-tight">Hello Alphazero</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+       {data && data.map((item) => (
+          <div key={item.id} className=" rounded-lg shadow-md p-4">
+            <h2 className="text-lg font-bold">{item.title}</h2>
+         </div>
+       ))}
+
+    </div>
     </div>
   );
 }
